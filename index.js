eb14dd5edd7cc2048822858d7f7dfb08a5a40775
fangpunpun <r00832r@gmail.com>
2022-09-19 11:51:01 +0800

    [feat] 新增本地存儲功能

--- index.js
+++ index.js
@@ -6,39 +6,55 @@ const deletedBtn = document.getElementById('deletedBtn')
 const deletedBtn2 = document.getElementById('deletedBtn2')
 const list = document.getElementById('list')
 
-const listContent = []
+
 function render(){
     let htmlStr = ''
     listContent.forEach(function(item,i){
         htmlStr += `
         <div class="item">
-            <div>
-                <h2>待辦 ${i+1}</h2>
-                <p>內容：${item.content}</p>
-                <p>時間：${item.date} ${item.time}</p>
-            </div>
+        <div>
+        <h2>待辦 ${i+1}</h2>
+        <p>內容：${item.content}</p>
+        <p>時間：${item.date} ${item.time}</p>
+        </div>
         </div>
         `
     })
     if(htmlStr == '') list.innerHTML = '<p>暫無待辦事項</p>'
     else list.innerHTML = htmlStr
 }
-addedBtn.addEventListener('click',function(){
-    // console.log(content.value)
-    console.log(date.value)
-    console.log(time.value)
-    listContent.push({
-        content: content.value,
-        date: date.value,
-        time: time.value
-    })      //新增一項資料進list
-    render()
-})
-deletedBtn.addEventListener('click',function(){
-    listContent.shift()
-    render()
-})
-deletedBtn2.addEventListener('click',function(){
-    listContent.pop()
-    render()
-})
+
+let listContent = null;
+
+function InitListContent(){
+    listContent = localStorage.getItem("listContent");
+    if(listContent == null) listContent = [];
+    else{
+        listContent = JSON.parse(listContent);
+        render()
+    }    
+}
+
+function SetEventListener(){
+    addedBtn.addEventListener('click',function(){
+        listContent.push({
+            content: content.value,
+            date: date.value,
+            time: time.value
+        })
+        localStorage.setItem( "listContent",JSON.stringify(listContent))
+        render()
+    })
+    deletedBtn.addEventListener('click',function(){
+        listContent.shift()
+        localStorage.setItem( "listContent",JSON.stringify(listContent))
+        render()
+    })
+    deletedBtn2.addEventListener('click',function(){
+        listContent.pop()
+        localStorage.setItem( "listContent",JSON.stringify(listContent))
+        render()
+    })
+}
+InitListContent();
+SetEventListener();
